ab71e9047d565b05ab872b932bfa4a23f0b2387d
Mathew <xuwanjin1991@Gmail.com>
2018-07-25 09:48:47 +0800

    add pages and moidfy register
    Signed-off-by: Mathew <xuwanjin1991@Gmail.com>

--- MyBlog/Blog/MySql/insert_user.sql
+++ MyBlog/Blog/MySql/insert_user.sql
@@ -1,7 +1,8 @@
+DROP TABLE IF EXISTS blog_user;
 CREATE TABLE blog_user(
     user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (user_id),
-    user_name VARCHAR(20) NOT NULL,
+    username VARCHAR(20) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     phone_number VARCHAR(50)
--- MyBlog/Blog/auth.py
+++ MyBlog/Blog/auth.py
@@ -15,40 +15,47 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 def register():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        passwd = request.form['password']
         email = request.form['email']
         db = get_db()
         error = list()
         if not username:
             error.extend('Username is required')
-        if not password:
+        if not passwd:
             error.extend('Password is required')
         if not email:
             error.extend('Email is required')
         if len(error) >= 1:
             raise RuntimeError
-        cursor = db.cursor().execute("CREATE DATABASE IF NOT EXISTS blog_user_db;")
-        from flask.cli import current_app 
+        cursor = db.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS blog_user_db;")
+        from flask.cli import current_app
         with current_app.open_resource("MySql/insert_user.sql") as f:
             sql = f.read().decode('utf8').split(';')
-            del sql[len(sql)-1]
-            db.cursor().execute(
+            del sql[len(sql) - 1]
+            cursor.execute(
                 sql[0]
             )
-        user_query = db.cursor().execute(
-            'SELECT id FROM user WHERE username = {}'.format(username)).fetchone()
-        email_query = db.cursor().execute(
-            'SELECT id FROM user WHERE email = {}'.format(email)).fetchone()
-        if error is None:
+            cursor.execute(
+                sql[1]
+            )
+            db.commit()
+        # check the account 
+        # user_query = db.cursor().execute(
+        #     'SELECT user_id FROM blog_user WHERE username = {}'.format(username)).fetchone()
+        # email_query = db.cursor().execute(
+        #     'SELECT user_id FROM blog_user WHERE email = {}'.format(email)).fetchone()
+        if error == []:
             db.cursor().execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)', (
-                    username, generate_password_hash(password))
+                'INSERT INTO blog_user(user_id, username, email, password, phone_number) VALUES ({}, "{}" , "{}" , "{}", "15902171045");'.format(
+                    'NULL', username, email, passwd)
             )
             db.commit()
             return redirect(url_for('auth.login'))
 
         flash(error)
     return render_template('auth/register.html')
+
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
@@ -57,25 +64,32 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.cursor().execute(
-            'SELECT * FROM user WHERE username = {}'.format(username)
-        ).fetchone()
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        cursor = db.cursor()
+        user = cursor.execute(
+            'SELECT * FROM blog_user WHERE username = "{}";'.format(username)
+        )
+        result = cursor.fetchone()
+        if result is None:
+            error = 'Incorrect username.' #the account does't exist, please check it
+        elif result[3] != password:
             error = 'Incorrect password.'
-        
+
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            session['user_id'] = result[0]
+            return redirect(url_for('auth.index'))
         flash(error)
     return render_template('auth/login.html')
+
+@bp.route('/index')
+def index():
+    return redirect(url_for('index'))
 
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
 
 def login_required(view):
     @functools.wraps(view)
@@ -83,5 +97,5 @@ def login_required(view):
         if g.user is None:
             return redirect(url_for('auth.login'))
         return view(**kwargs)
-    return wraped_view
 
+    return wraped_view
